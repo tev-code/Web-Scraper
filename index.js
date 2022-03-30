4b2254822061e64fdcd3d177ae0ed1b1a6965d1d
Tevin <tevin@trade245.com>
2022-03-30 18:40:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
+
+const url = 'https://www.theguardian.com/uk'
+
+
+// Get URL through axios and select specific data with cheerio
+axios(url)
+.then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const articles = []
+
+
+
+    $('.fc-item__title', html).each(function() {
+      const title = $(this).text()
+      const url = $(this).find('a').attr('href')
+      articles.push({
+          title,
+          url
+      })
+    })
+    console.log(articles)
+    }).catch(err => console.log(err))
+
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
